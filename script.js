5d5563f22c1c4bf66be6780584359186e279b208
Alan Moreno <alan.moreno@hotmail.com>
2022-10-29 00:05:37 -0300

    MVP status

--- script.js
+++ script.js
@@ -1,32 +1,16 @@
-function convertMessage(msg) {
-    var chars = [
-        { htmlDec: '&#32', utf8: '+' },
-        { htmlDec: '+', utf8: '%2B' },
-        { htmlDec: '%', utf8: '%2A' },
-        { htmlDec: '\n', utf8: '%0a' }
-    ];
-    console.log(chars);
-    let message;
-    for(let i = 0; i < msg.length; i++) {
-        if(chars.indexOf(msg.indexOf(i))>=0){
-            message.push(msg.indexOf(i));
-        }else{
-
-        }
-    }
-
-}
-
 function genLink() {
     let phone = document.getElementById('phone-number').value;
+    phone = phone.replace(/([^\d])+/gim, '');
     let msg = document.getElementById('message').value;
-    convertMessage(msg);
+    let message = encodeURIComponent(msg);
     let link = 'http://wa.me/55'+phone+'?text='+message;
     console.log(link);
     document.getElementById('result-box').classList.remove('display-none');
+    document.getElementById('a-link').setAttribute('href', link);
+    document.getElementById('a-link').innerHTML = link;
 }
 
 function getCopy() {
-    let link = document.getElementById('resulting-link').innerText;
+    let link = document.getElementById('p-link').innerText;
     navigator.clipboard.writeText(link);
 }
